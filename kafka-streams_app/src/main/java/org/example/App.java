54152c6c55fe73d9986d20ecd11d19bdc01a376b
432i <aevi1313@gmail.com>
2022-10-06 23:27:44 -0300

    windowing is now working correctly and added logic to iot (need refactor)

--- kafka-streams_app/src/main/java/org/example/App.java
+++ kafka-streams_app/src/main/java/org/example/App.java
@@ -29,7 +29,6 @@ public class App
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-test");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "34.176.255.191:9092");
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Elige el dataset:");
         System.out.println("1.- Twitter");
@@ -55,16 +54,18 @@ public class App
 
         KStream<String, String> twitterA = builder.stream("twitterA",
                 Consumed.with(stringSerde, stringSerde).withTimestampExtractor(new StreamsTimestampExtractor.myTimestampExtractor()) )
+                .mapValues(value -> splitValue(value))
                 .peek((key, value) -> System.out.println("twitterA key: " + key + " , value: " + value));;
 
         KStream<String, String> twitterB = builder.stream("twitterB",
                 Consumed.with(stringSerde, stringSerde).withTimestampExtractor(new StreamsTimestampExtractor.myTimestampExtractor()) )
+                .mapValues(value -> splitValue(value))
                 .peek((key, value) -> System.out.println("twitterB key: " + key + " , value: " + value));
 
         System.out.println("Initiating join operation");
-        ValueJoiner<String, String, String> valueJoiner = (leftValue, rightValue) -> leftValue + " " + rightValue;
+        ValueJoiner<String, String, Float> valueJoiner = (leftValue, rightValue) -> Float.parseFloat(leftValue) + Float.parseFloat(rightValue);
 
-        KStream<String, String> combinedStream =
+        KStream<String, Float> combinedStream =
                 twitterA.join(
                         twitterB,
                         valueJoiner,
@@ -75,18 +76,26 @@ public class App
 
         System.out.println("Initiating tumbling window operation");
         combinedStream.groupByKey()
-                .windowedBy(TimeWindows.of(Duration.ofSeconds(30)))
-                .aggregate(() -> 0,
-                        (key, value, total) -> total + value.length(),
-                        Materialized.with(Serdes.String(), Serdes.Integer()))
-                        .suppress(untilWindowCloses(unbounded()))
+                .windowedBy(TimeWindows.of(Duration.ofSeconds(40)))
+                .aggregate(() -> 0.0f,
+                        (key, value, total) -> total/2 + value/2,
+                        Materialized.with(Serdes.String(), Serdes.Float()))
+                        //.suppress(untilWindowCloses(unbounded()))
                 .toStream()
-                .map((wk, value) -> KeyValue.pair(wk.key(),value))
-                .peek((key, value) -> System.out.println("Outgoing record - key " +key +", number of characters of the joined streams: " + value));
+                .map((wk, value) -> KeyValue.pair(wk.key(), value))
+                .peek((key, value) -> System.out.println("FINAL WINDOW - key " +key +", average temperature in celsius: " + value));
 
 
         KafkaStreams streams = new KafkaStreams(builder.build(), props);
         streams.start();
+    }
+
+
+
+    public static String splitValue(String value){
+        //asumming an input of 2707176363363894:"2021-02-07 00:03:19,1612656199,63.3,17.4"
+        String[] parts = value.split(",");
+        return parts[3];
     }
 
     public static void iot_topic_connection(Properties props) throws IOException {
@@ -116,15 +125,14 @@ public class App
                         );
 
         System.out.println("Initiating tumbling window operation");
-        Duration windowSize = Duration.ofSeconds(30);
         combinedStream.groupByKey()
-                .windowedBy(TimeWindows.of(windowSize))
+                .windowedBy(TimeWindows.of(Duration.ofSeconds(15)))
                 .aggregate(() -> 0,
                         (key, value, total) -> total + value.length(),
                         Materialized.with(Serdes.String(), Serdes.Integer()))
-                .suppress(untilWindowCloses(unbounded()))
+                //.suppress(untilWindowCloses(unbounded()))
                 .toStream()
-                .map((wk, value) -> KeyValue.pair(wk.key(),value))
+                .map((wk, value) -> KeyValue.pair(wk.key(), value))
                 .peek((key, value) -> System.out.println("Outgoing record - key " +key +", number of characters of the joined streams: " + value));
 
         KafkaStreams streams = new KafkaStreams(builder.build(), props);
@@ -145,6 +153,29 @@ public class App
         KStream<String, String> logB = builder.stream("logB",
                         Consumed.with(stringSerde, stringSerde).withTimestampExtractor(new StreamsTimestampExtractor.myTimestampExtractor()) )
                 .peek((key, value) -> System.out.println("logB key: " + key + " , value: " + value));
+        System.out.println("Initiating join operation");
+        ValueJoiner<String, String, String> valueJoiner = (leftValue, rightValue) -> leftValue + " " + rightValue;
+
+        KStream<String, String> combinedStream =
+                logA.join(
+                                logB,
+                                valueJoiner,
+                                JoinWindows.of(Duration.ofMinutes(10)),
+                                StreamJoined.with(Serdes.String(), stringSerde, stringSerde))
+                        .peek((key, value) -> System.out.println("Stream-Stream Join: record key " + key + ", value:" + value)
+                        );
+
+        System.out.println("Initiating tumbling window operation");
+        combinedStream.groupByKey()
+                .windowedBy(TimeWindows.of(Duration.ofSeconds(15)))
+                .aggregate(() -> 0,
+                        (key, value, total) -> total + value.length(),
+                        Materialized.with(Serdes.String(), Serdes.Integer()))
+                //.suppress(untilWindowCloses(unbounded()))
+                .toStream()
+                .map((wk, value) -> KeyValue.pair(wk.key(),value))
+                .peek((key, value) -> System.out.println("Outgoing record - key " +key +", number of characters of the joined streams: " + value));
+
 
         KafkaStreams streams = new KafkaStreams(builder.build(), props);
         streams.start();
