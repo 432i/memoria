00dcf9953774ce6598a3e809a69a196cf844bcca
432i <aevi1313@gmail.com>
2022-11-12 21:34:34 -0300

    advances in the join and tumbling window implementation
    next: needs to make join and tumbling window function together

--- beam-app/src/main/java/org/example/App.java
+++ beam-app/src/main/java/org/example/App.java
@@ -6,8 +6,14 @@ import org.apache.beam.sdk.io.kafka.KafkaIO;
 import org.apache.beam.sdk.io.kafka.KafkaRecord;
 import org.apache.beam.sdk.extensions.joinlibrary.Join;
 import org.apache.beam.sdk.transforms.*;
+import org.apache.beam.sdk.transforms.join.CoGbkResult;
+import org.apache.beam.sdk.transforms.join.CoGroupByKey;
+import org.apache.beam.sdk.transforms.join.KeyedPCollectionTuple;
+import org.apache.beam.sdk.transforms.windowing.FixedWindows;
+import org.apache.beam.sdk.transforms.windowing.Window;
 import org.apache.beam.sdk.values.*;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.joda.time.Duration;
 
 import java.io.IOException;
 import java.util.Properties;
@@ -58,19 +64,33 @@ public class App
                 MapElements.into(TypeDescriptors.kvs(TypeDescriptors.strings(), TypeDescriptors.strings()))
                         .via((KafkaRecord<String, String> record) -> KV.of(record.getKV().getKey(), splitValue(record.getKV().getValue(),0)))
         );
-        //al parecer hay que implementar el windowing para que el join funcione
-        PCollection<KV<String, KV<String, String>>> combinedCollections = Join.innerJoin(wrdA, wrdB);
 
-        combinedCollections.apply( //method to print converted records
-                MapElements.via(
-                        new SimpleFunction<KV<String, KV<String, String>>, KV<String, KV<String, String>>>() {
-                            @Override
-                            public KV<String, KV<String, String>> apply(KV<String, KV<String, String>> record) {
-                                System.out.println(
-                                        ", 1:"+record.getKey()+", 0:"+record.getValue());
-                                return record;
-                            }
-                        }));
+        //windowing function (fixed time function == tumbling window). Need to define a window
+        //in order to apply transforms
+        TupleTag<String> wrdA_tag = new TupleTag<>();
+        TupleTag<String> wrdB_tag = new TupleTag<>();
+        //join by keys
+        PCollection<KV<String, CoGbkResult>> results =
+                KeyedPCollectionTuple.of(wrdA_tag, wrdA)
+                        .and(wrdB_tag, wrdB)
+                        .apply(CoGroupByKey.create());
+
+        PCollection<KV<String, String>> wrdA_window = wrdA.apply(
+                Window.<KV<String, String>>into(FixedWindows.of(Duration.standardSeconds(60))));
+
+        //al parecer hay que implementar el windowing para que el join funcione
+        //PCollection<KV<String, KV<String, String>>> combinedCollections = Join.innerJoin(wrdA, wrdB);
+
+        //combinedCollections.apply( //method to print converted records
+        //        MapElements.via(
+        //                new SimpleFunction<KV<String, KV<String, String>>, KV<String, KV<String, String>>>() {
+        //                    @Override
+        //                    public KV<String, KV<String, String>> apply(KV<String, KV<String, String>> record) {
+        //                        System.out.println(
+        //                                ", 1:"+record.getKey()+", 0:"+record.getValue());
+        //                        return record;
+        //                    }
+        //                }));
 
         //PCollection<String> wrdB = pCollectionB.apply(
         //        ParDo.of(new StreamOperation()));
