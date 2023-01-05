2887c73af06116047daf2d404e65323c893206d5
432i <aevi1313@gmail.com>
2023-01-05 00:16:52 -0300

    iot pipeline finished
    iot pipeline finished - twitter and log pipelines are now initiated

--- spark-app/src/main/java/org/example/App.java
+++ spark-app/src/main/java/org/example/App.java
@@ -93,57 +93,184 @@ public class App {
                 functions.window(col("timestampA"), "15 seconds"),
                 col("keyA")
         ).avg("valueA");
-        windowedAvg = windowedAvg.withColumnRenamed("avg(valueA)", "new_value");
-        //1:2021-02-07 00:03:19,1612656199,63.3,17.1
-        //1:2021-02-07 00:03:19,1612656199,63.3,17.2
-        // Start running the query that prints the data getting from Kafka 'iotA' topic
-        StreamingQuery query = iotA.writeStream()
-                .outputMode("append")
-                .format("console")
-                .start();
-        StreamingQuery query1 = iotB.writeStream()
-                .outputMode("append")
-                .format("console")
-                .start();
-        StreamingQuery query2 = windowedAvg
-                .selectExpr("CAST(keyA AS STRING)", "CAST(new_value AS STRING)")
-                .writeStream()
-                .format("kafka")
-                .option("checkpointLocation", "C:\\Users\\aevi1\\Downloads")
-                .option("kafka.bootstrap.servers", IP)
-                .option("topic", "iot_output")
-                .start();
-        /*StreamingQuery query2 = mapped_iotA.writeStream()
-                .outputMode("append")
-                .format("console")
-                .start();*/
-        StreamingQuery query3 = mapped_iotB.writeStream()
-                .outputMode("append")
-                .format("console")
-                .start();
-        StreamingQuery query4 = joined_streams.writeStream()
-                .outputMode("append")
-                .format("console")
-                .start();
-        StreamingQuery query5 = windowedAvg.writeStream()
-                .outputMode("append")
-                .format("console")
-                .start();
-        //Getting the data value as String
-        query.awaitTermination();
-        query1.awaitTermination();
-        query3.awaitTermination();
-        query4.awaitTermination();
-        query5.awaitTermination();
-        query2.awaitTermination();
+        windowedAvg = windowedAvg.withColumnRenamed("avg(valueA)", "value");
+        windowedAvg = windowedAvg.withColumnRenamed("keyA", "key");
+
+        windowedAvg
+        .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+        .writeStream()
+        .format("kafka")
+        .format("console")
+        .outputMode("append")
+        .option("kafka.bootstrap.servers", IP)
+        .option("topic", "iot_output")
+        .option("checkpointLocation", "C:\\Users\\aevi1\\Downloads")
+        .start();
+        //joined_streams.writeStream()
+        //        .outputMode("append")
+        //        .format("console")
+        //        .start().awaitTermination();
+        spark.streams().awaitAnyTermination();
     }
 
     public static void twitter_topic_connection(String IP) throws Exception {
+        System.out.println( "Initiating connection with twitter topic" );
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("App")
+                .master("local")
+                .getOrCreate();
+        spark.conf().set("spark.sql.shuffle.partitions",2);
+        spark.sparkContext().setLogLevel("ERROR");
+        Dataset<Row> twitterA = spark
+                .readStream()
+                .format("kafka")
+                .option("kafka.bootstrap.servers", IP)
+                .option("subscribe", "twitterA")
+                .load();
+        twitterA = twitterA.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)",
+                "CAST(topic AS STRING)", "CAST(timestamp AS TIMESTAMP)");
+        twitterA = twitterA.withColumnRenamed("key", "keyA");
+        twitterA = twitterA.withColumnRenamed("value", "valueA");
+        twitterA = twitterA.withColumnRenamed("topic", "topicA");
+        twitterA = twitterA.withColumnRenamed("timestamp", "timestampA");
+        Dataset<Row> twitterB = spark
+                .readStream()
+                .format("kafka")
+                .option("kafka.bootstrap.servers", IP)
+                .option("subscribe", "twitterB")
+                .load();
+        twitterB = twitterB.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)",
+                "CAST(topic AS STRING)", "CAST(timestamp AS TIMESTAMP)");
+        twitterB = twitterB.withColumnRenamed("key", "keyB");
+        twitterB = twitterB.withColumnRenamed("value", "valueB");
+        twitterB = twitterB.withColumnRenamed("topic", "topicB");
+        twitterB = twitterB.withColumnRenamed("timestamp", "timestampB");
 
+
+        Dataset<Row> mapped_twitterA = twitterA.map((MapFunction<Row, Row>) row ->
+                {return RowFactory.create(row.get(0), splitValue((String) row.get(1), 3), row.get(2), row.get(3));},
+                RowEncoder.apply(twitterA.schema()));
+        mapped_twitterA = mapped_twitterA.selectExpr("CAST(keyA AS STRING)", "CAST(valueA AS FLOAT)",
+                "CAST(topicA AS STRING)", "CAST(timestampA AS TIMESTAMP)");
+        Dataset<Row> mapped_twitterB = twitterB.map((MapFunction<Row, Row>) row ->
+                {return RowFactory.create(row.get(0), splitValue((String) row.get(1), 3), row.get(2), row.get(3));},
+                RowEncoder.apply(twitterB.schema()));
+        mapped_twitterB = mapped_twitterB.selectExpr("CAST(keyB AS STRING)", "CAST(valueB AS FLOAT)",
+                "CAST(topicB AS STRING)", "CAST(timestampB AS TIMESTAMP)");
+
+        Dataset<Row> joined_streams = mapped_twitterA
+                .join(mapped_twitterB, mapped_twitterA.col("keyA").equalTo(mapped_twitterB.col("keyB")));
+
+        joined_streams = joined_streams .map(
+                (MapFunction<Row, Row>) row ->
+                {return RowFactory.create(row.get(0), avrg((Float)row.get(1), (Float)row.get(5)), row.get(2), row.get(3), row.get(4), row.get(5), row.get(6), row.get(7));},
+                RowEncoder.apply(joined_streams.schema())
+        );
+        joined_streams = joined_streams.withWatermark("timestampA", "1 minute");
+
+        Dataset<Row> windowedAvg = joined_streams.groupBy(
+                functions.window(col("timestampA"), "15 seconds"),
+                col("keyA")
+        ).avg("valueA");
+        windowedAvg = windowedAvg.withColumnRenamed("avg(valueA)", "value");
+        windowedAvg = windowedAvg.withColumnRenamed("keyA", "key");
+
+        windowedAvg
+                .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+                .writeStream()
+                .format("kafka")
+                .format("console")
+                .outputMode("append")
+                .option("kafka.bootstrap.servers", IP)
+                .option("topic", "iot_output")
+                .option("checkpointLocation", "C:\\Users\\aevi1\\Downloads")
+                .start();
+        joined_streams.writeStream()
+                .outputMode("append")
+                .format("console")
+                .start().awaitTermination();
+        spark.streams().awaitAnyTermination();
     }
 
     public static void log_topic_connection(String IP) throws Exception {
+        System.out.println( "Initiating connection with log topic" );
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("App")
+                .master("local")
+                .getOrCreate();
+        spark.conf().set("spark.sql.shuffle.partitions",2);
+        spark.sparkContext().setLogLevel("ERROR");
+        Dataset<Row> logA = spark
+                .readStream()
+                .format("kafka")
+                .option("kafka.bootstrap.servers", IP)
+                .option("subscribe", "logA")
+                .load();
+        logA = logA.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)",
+                "CAST(topic AS STRING)", "CAST(timestamp AS TIMESTAMP)");
+        logA = logA.withColumnRenamed("key", "keyA");
+        logA = logA.withColumnRenamed("value", "valueA");
+        logA = logA.withColumnRenamed("topic", "topicA");
+        logA = logA.withColumnRenamed("timestamp", "timestampA");
+        Dataset<Row> logB = spark
+                .readStream()
+                .format("kafka")
+                .option("kafka.bootstrap.servers", IP)
+                .option("subscribe", "logB")
+                .load();
+        logB = logB.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)",
+                "CAST(topic AS STRING)", "CAST(timestamp AS TIMESTAMP)");
+        logB = logB.withColumnRenamed("key", "keyB");
+        logB = logB.withColumnRenamed("value", "valueB");
+        logB = logB.withColumnRenamed("topic", "topicB");
+        logB = logB.withColumnRenamed("timestamp", "timestampB");
 
+
+        Dataset<Row> mapped_logA = logA.map((MapFunction<Row, Row>) row ->
+                {return RowFactory.create(row.get(0), splitValue((String) row.get(1), 2), row.get(2), row.get(3));},
+                RowEncoder.apply(logA.schema()));
+        mapped_logA = mapped_logA.selectExpr("CAST(keyA AS STRING)", "CAST(valueA AS FLOAT)",
+                "CAST(topicA AS STRING)", "CAST(timestampA AS TIMESTAMP)");
+        Dataset<Row> mapped_logB = logB.map((MapFunction<Row, Row>) row ->
+                {return RowFactory.create(row.get(0), splitValue((String) row.get(1), 2), row.get(2), row.get(3));},
+                RowEncoder.apply(logB.schema()));
+        mapped_logB = mapped_logB.selectExpr("CAST(keyB AS STRING)", "CAST(valueB AS FLOAT)",
+                "CAST(topicB AS STRING)", "CAST(timestampB AS TIMESTAMP)");
+
+        Dataset<Row> joined_streams = mapped_logA
+                .join(mapped_logB, mapped_logA.col("keyA").equalTo(mapped_logB.col("keyB")));
+
+        joined_streams = joined_streams .map(
+                (MapFunction<Row, Row>) row ->
+                {return RowFactory.create(row.get(0), avrg((Float)row.get(1), (Float)row.get(5)), row.get(2), row.get(3), row.get(4), row.get(5), row.get(6), row.get(7));},
+                RowEncoder.apply(joined_streams.schema())
+        );
+        joined_streams = joined_streams.withWatermark("timestampA", "1 minute");
+
+        Dataset<Row> windowedAvg = joined_streams.groupBy(
+                functions.window(col("timestampA"), "15 seconds"),
+                col("keyA")
+        ).avg("valueA");
+        windowedAvg = windowedAvg.withColumnRenamed("avg(valueA)", "value");
+        windowedAvg = windowedAvg.withColumnRenamed("keyA", "key");
+
+        windowedAvg
+                .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+                .writeStream()
+                .format("kafka")
+                .format("console")
+                .outputMode("append")
+                .option("kafka.bootstrap.servers", IP)
+                .option("topic", "iot_output")
+                .option("checkpointLocation", "C:\\Users\\aevi1\\Downloads")
+                .start();
+        joined_streams.writeStream()
+                .outputMode("append")
+                .format("console")
+                .start().awaitTermination();
+        spark.streams().awaitAnyTermination();
     }
 
     //helper methods
