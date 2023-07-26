072043342ea37861351e7658faf5d513d19a03fa
432i <aevi1313@gmail.com>
2023-07-25 23:41:56 -0400

    Revert "quick fix to previous commit (added deleted imports)"
    This reverts commit afe1417c6d2c15b1d4216bedce293195c6fd12c9.

--- kafka-streams_app/src/main/java/org/example/App.java
+++ kafka-streams_app/src/main/java/org/example/App.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
