151f6cba80abce6d6a2de47783a701e06ae0d23a
432i <aevi1313@gmail.com>
2023-07-25 23:10:46 -0400

    added new columns to final result and quick fixes

--- latency_script/latency_calculator.py
+++ latency_script/latency_calculator.py
@@ -7,7 +7,9 @@ default_split_str = "!!432&%$(())#"
 iot_split_str = ","
 
 def parse_input_data(input_data):
+    global CURRENT_TOPIC
     input_topic = str(input_data.iloc[0]['topic'])
+    CURRENT_TOPIC = input_topic[:-1]
     values_list = input_data['value'].tolist()
     split_rows = lambda row: row.split(iot_split_str)[-1] if "iot" in input_topic else row.split(default_split_str)[-1]
     ids_list = []
@@ -31,23 +33,20 @@ def calculate_avg_timestamp(tt1, tt2):
     return ((int(tt1) + int(tt2)) / 2)
 
 def clean_and_process(parsed_input, parsed_output):
-    avg_input_timestamps = []
-    final_latencies      = []
+    parsed_output['input_timestampA'], parsed_output['input_timestampB'] = "", ""
+    parsed_output['avg_input_timestamp'], parsed_output['final_timestamp'] = "", ""
     for index, row in parsed_output.iterrows():
         id_topicA, id_topicB = row['id_topicA'], row['id_topicB']
         rowA, rowB = parsed_input.loc[parsed_input['input_row_id'] == id_topicA], parsed_input.loc[parsed_input['input_row_id'] == id_topicB]
         
-        avg_input_timestamp = calculate_avg_timestamp(rowA['input_timestamp'], rowB['output_timestamp'])
-        avg_input_timestamps.append(avg_input_timestamp)
+        avg_input_timestamp = calculate_avg_timestamp(rowA['input_timestamp'], rowB['input_timestamp'])
 
         final_latency = int(row['output_timestamp']) - avg_input_timestamp
-        final_latencies.append(final_latency)
 
-        print(parsed_input.loc[parsed_input['input_row_id'] == id_topicA])
-        print(id_topicA)
-        break
-    parsed_output['avg_input_timestamp'], parsed_output['final_timestamp'] = avg_input_timestamps, final_latencies
-    result = parsed_output.drop('offset', axis=1)
+        parsed_output.at[index, 'input_timestampA'], parsed_output.at[index, 'input_timestampB'] = rowA['input_timestamp'].values[0], rowB['input_timestamp'].values[0]
+        parsed_output.at[index, 'avg_input_timestamp'], parsed_output.at[index, 'final_timestamp'] = avg_input_timestamp, final_latency
+
+    result = parsed_output.drop(['offset', 'output_key', 'output_topic'], axis=1)
     return result
 
 input_data = pd.read_csv("input_timestamps.csv", sep=";", dtype=dtypes_dict, skiprows=[1])
@@ -55,8 +54,5 @@ output_data = pd.read_csv("output_timestamps.csv", sep=";", dtype=dtypes_dict)
 parsed_input = parse_input_data(input_data)
 parsed_output = parse_output_data(output_data)
 result = clean_and_process(parsed_input, parsed_output)
-result.to_csv('timestamps_result', sep=';', encoding='utf-8')
-print(parsed_input)
-print(parsed_output)
-print(result.loc[0])
-print(result.columns)
+result.to_csv(f'{CURRENT_TOPIC}_timestamps_result', sep=';', encoding='utf-8')
+print(result)
